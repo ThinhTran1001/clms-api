e8eeb4839a4be725b34c3714aa5c50fe14be96f2
TranDucThinhK17HL <thinhtdhe173229@fpt.edu.vn>
2024-09-15 17:29:33 +0700

    create .env file

--- src/main/java/vn/threeluaclmsapi/ThreeluaClmsApiApplication.java
+++ src/main/java/vn/threeluaclmsapi/ThreeluaClmsApiApplication.java
@@ -1,12 +1,23 @@
 package vn.threeluaclmsapi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class ThreeluaClmsApiApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
+        System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
+        System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
+
         SpringApplication.run(ThreeluaClmsApiApplication.class, args);
     }
 
