9c40117bf0221132af5fb54b10b83bfe7809b2c7
TranDucThinhK17HL <thinhtdhe173229@fpt.edu.vn>
2024-09-15 10:13:49 +0700

    init project

---
+++ src/main/java/vn/threeluaclmsapi/ThreeluaClmsApiApplication.java
@@ -0,0 +1,13 @@
+package vn.threeluaclmsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreeluaClmsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreeluaClmsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/threeluaclmsapi/ThreeluaClmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.threeluaclmsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreeluaClmsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
