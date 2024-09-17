c41f610cee68016d3459dadc1475528b4049b139
TranDucThinhK17HL <thinhtdhe173229@fpt.edu.vn>
2024-09-18 00:53:32 +0700

    fix validation

--- src/main/java/vn/threeluaclmsapi/dto/request/CreateClassroomRequest.java
+++ src/main/java/vn/threeluaclmsapi/dto/request/CreateClassroomRequest.java
@@ -1,11 +1,18 @@
 package vn.threeluaclmsapi.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
+import vn.threeluaclmsapi.util.constant.ValidationConstant;
 
 @Getter
 public class CreateClassroomRequest {
 
+    @NotBlank(message = "Tên lớp không được để trống!")
+    @Pattern(regexp = "^[A-Z]{2}\\d{4}$", message = "Tên lớp không hợp lệ, phải có 2 chữ cái viết hoa và 4 số")
     private String classroomName;
 
+    @Min(value = ValidationConstant.MIN_CLASSROOM_CAPACITY, message = "Lớp học cần có ít nhất 25 học sinh!")
     private Integer capacity;
 }
