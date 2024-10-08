81d7d3128a30d8e8108c3f9e2d497dc7eb8233da
TranDucThinhK17HL <thinhtdhe173229@fpt.edu.vn>
2024-10-08 22:07:07 +0700

    add spring security, jwt dependency

--- src/main/java/vn/threeluaclmsapi/controller/AuthController.java
+++ src/main/java/vn/threeluaclmsapi/controller/AuthController.java
@@ -1,4 +1,10 @@
 package vn.threeluaclmsapi.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
 }
